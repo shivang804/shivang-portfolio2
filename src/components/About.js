bf8f1ba073de5517da84f6038b05df185b35cd6e
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-26 18:16:59 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
+import '../styles/About.css'
 
-function About() {
-  return (
-    <section id = 'about'>
-        <h2>About me</h2>
-        <p>I am beginner web developer</p>
-    </section>
-  )
-}
+const About = () => {
+    return (
+        <>
+        <section id="about" className="about">
+            <h2>About Me</h2>
+            <p>Hello! I'm SHIVANG, a Software Devloper. I have a passion for Website Development. I enjoy working on projects that challenge me and allow me to grow my skillset.</p>
+            <p>In my free time, I love listening music. I'm constantly learning new things and looking for opportunities to apply my skills in meaningful ways.</p>
+        
+        </section>
+        <section id="skills" className="skills">
+            <h2>My Skills</h2>
+            <ul className="skills-list">
+                <li>JavaScript</li>
+                <li>React.js</li>
+                <li>HTML & CSS</li>
+                <li>Git & GitHub</li>
+                <li>Responsive Design</li>
+                <li>Node.js</li>
+            </ul>
+        </section>
 
-export default About
+        
+        </>
+    );
+};
+
+export default About;
