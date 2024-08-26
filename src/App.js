1646756034e3febba43eb01496d6268af35eac73
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-26 18:22:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
+import Hero from './components/Hero';
 import About from './components/About';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Education from './components/Education';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <About />
-      <Projects />
-      <Contact />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Hero />
+            <About />
+            <Skills />
+            <Experience />
+            <Projects />
+            <Education />
+            <Contact />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
