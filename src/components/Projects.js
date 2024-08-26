f7676d94a087cd7ccf6982b548994a94fa9452b6
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-26 18:16:09 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import '../styles/Projects.css'
 
-function Projects() {
-  return (
-    <section id="projects">
-      <h2>Projects</h2>
-      <ul>
-        <li>Project 1</li>
-        <li>Project 2</li>
-        <li>Project 3</li>
-      </ul>
-    </section>
-  );
-}
+const Projects = () => {
+    return (
+        <section id="projects" className="projects">
+            <h2>Projects</h2>
+            <div className="project">
+                <h3>Portfolio Website</h3>
+                <p>A simple portfolio website about me.</p>
+            </div>
+            <div className="project">
+                <h3>FreeBookPost an E-commerce Platform</h3>
+                <p>An online books downloading platform.</p>
+            </div>
+        </section>
+    );
+};
 
 export default Projects;
