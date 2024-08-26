2e9aa2da6fb87aa7e85d352ee16b5ee6ef2f8fd7
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-26 18:16:31 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import '../styles/Contact.css'
 
-function Contact() {
-  return (
-    <section id="contact">
-      <h2>Contact</h2>
-      <p>You can reach me at: <a href="mailto:sharmashivang804@gmail.com">sharmashivang804@gmail.com</a></p>
-    </section>
-  );
-}
+const Contact = () => {
+    return (
+        <section id="contact" className="contact">
+            <h2>Contact Me</h2>
+            <p>Email: <a href='mailto:sharmashivang804@gmail.com'>Gmail</a></p>
+            <p>LinkedIn: <a href='https://www.linkedin.com/in/shivang-741b781aa/'>LinkedIn</a></p>
+            <p>GitHub: <a href='https://github.com/shivang804'>GitHub</a></p>
+            <a href='resume.pdf' download className="btn-resume">Download Resume</a>
+        </section>
+    );
+};
 
 export default Contact;
