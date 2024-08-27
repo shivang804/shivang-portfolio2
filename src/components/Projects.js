d18efbb09744d20a7befeef866052923ed1b516e
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-27 18:26:39 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,10 +7,12 @@ const Projects = () => {
             <h2>Projects</h2>
             <div className="project">
                 <h3>Portfolio Website</h3>
+                <img src='./1.png' alt= 'projectimage' className='portfolio_image'></img>
                 <p>A simple portfolio website about me.</p>
             </div>
             <div className="project">
                 <h3>FreeBookPost an E-commerce Platform</h3>
+                <img src='./21.png' alt= 'projectimage' className='portfolio_image'></img>
                 <p>An online books downloading platform.</p>
             </div>
         </section>
