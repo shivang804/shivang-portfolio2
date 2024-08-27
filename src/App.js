9f4e24a5e5dd121734e41b27135f51bfac7f9583
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-27 18:29:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
-import Skills from './components/Skills';
+//import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Education from './components/Education';
@@ -16,7 +16,7 @@ function App() {
             <Header />
             <Hero />
             <About />
-            <Skills />
+            {/*<Skills />*/}
             <Experience />
             <Projects />
             <Education />
