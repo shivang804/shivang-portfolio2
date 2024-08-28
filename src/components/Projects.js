5f41b5a5f18d89b960aa6ea0f3188e14c2a5dfcd
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-28 18:38:26 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,16 +5,29 @@ const Projects = () => {
     return (
         <section id="projects" className="projects">
             <h2>Projects</h2>
-            <div className="project">
-                <h3>Portfolio Website</h3>
-                <img src='./1.png' alt= 'projectimage' className='portfolio_image'></img>
-                <p>A simple portfolio website about me.</p>
+            <div className="about__content">
+          <div className="about__content-main">
+            <div className="about__content-details">
+              <p className="about__content-details-para">
+              <h3 className="project1">Portfolio Website</h3>
+              <img src='./1.png' alt= 'projectimage' className='portfolio_image'></img>
+              </p>
+              <h3 className="project2">FreeBookPost an E-commerce Platform</h3>
+              <img src='./21.png' alt= 'projectimage' className='portfolio_image'></img>
+
             </div>
-            <div className="project">
-                <h3>FreeBookPost an E-commerce Platform</h3>
-                <img src='./21.png' alt= 'projectimage' className='portfolio_image'></img>
-                <p>An online books downloading platform.</p>
+        
+          </div>
+          <div className="about__content-skills" id="">
+            <div className="project_skills">
+            <p className='description1'>A simple portfolio website about me.</p>
+            <a href='https://github.com/shivang804/shivang-portfolio' className="btn-resume">ProjectLink</a>
+            <p className='description2'>An online books downloading platform.</p>
+            <a href='https://github.com/shivang804/Shivang_freebookpost'className="btn-resume">ProjectLink</a>
             </div>
+          </div>
+        </div>
+    
         </section>
     );
 };
